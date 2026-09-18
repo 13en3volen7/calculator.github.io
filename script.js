74490355023e226a3daf25d93c2f239b2d95b849
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-18 21:57:28 +0700

    Add basic support for math operators

--- script.js
+++ script.js
@@ -4,6 +4,31 @@ function initialize() {
     attachButtonEvents(containerDiv);
 }
 
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, op) {
+    switch (op) {
+        case "+": return add(a, b);
+        case "−": return subtract(a, b);
+        case "×": return multiply(a, b);
+        case "÷": return divide(a, b);
+    }
+}
+
 function attachButtonEvents(containerDiv) {
     const optionsDiv = containerDiv.querySelector(".options");
 
