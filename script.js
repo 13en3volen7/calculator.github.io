98cd3c7cb985a82aed6661f1c552ec25c9fff623
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-18 23:32:38 +0700

    Support basic operations without error handling

--- script.js
+++ script.js
@@ -22,15 +22,35 @@ function divide(a, b) {
 
 function operate(a, b, op) {
     switch (op) {
-        case "+": return add(a, b);
-        case "−": return subtract(a, b);
-        case "×": return multiply(a, b);
-        case "÷": return divide(a, b);
+        case "+": return `${add(a, b)}`;
+        case "−": return `${subtract(a, b)}`;
+        case "×": return `${multiply(a, b)}`;
+        case "÷": return `${divide(a, b)}`;
     }
 }
 
 function attachButtonEvents(containerDiv) {
     const optionsDiv = containerDiv.querySelector(".options");
+    let firstNumber = "";
+    let secondNumber = "";
+    let operator = "";
+    let firstNumberIsNegative = false;
+    
+    function nameToSymbol(operatorName) {
+        switch (operatorName) {
+            case "plus": return "+";
+            case "minus": return "−";
+            case "multiply": return "×";
+            case "divide": return "÷";
+            case "equal": return "=";
+        }
+    }
+
+    function wipeData() {
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+    }
 
     optionsDiv.addEventListener("click", function (event) {
         const target = event.target;
@@ -40,16 +60,41 @@ function attachButtonEvents(containerDiv) {
         const className = target.className;
 
         if (className.startsWith("number")) {
-            contentDiv.textContent += className.slice(-1);
+            if (operator === "=") {
+                wipeData();
+                contentDiv.textContent = "";
+            }
+            const number = className.slice(-1);
+            contentDiv.textContent += number;
+            if (!operator) {
+                if (firstNumberIsNegative) {
+                    firstNumber = "-" + firstNumber;
+                    firstNumberIsNegative = false;
+                }
+                firstNumber += number;  
+            } else {
+                secondNumber += number;
+            }
         } else if (className.startsWith("operator")) {
-            if (className.endsWith("plus")) {
-                contentDiv.textContent += "+";
-            } else if (className.endsWith("minus")) {
-                contentDiv.textContent += "−";
-            } else if (className.endsWith("multiply")) {
-                contentDiv.textContent += "×";
-            } else if (className.endsWith("divide")) {
-                contentDiv.textContent += "÷";
+            console.log(firstNumber, secondNumber, operator);
+            if (firstNumber && secondNumber && operator) {
+                const result = operate(+firstNumber, +secondNumber, operator);
+                firstNumber = result;
+                secondNumber = "";
+                contentDiv.textContent = result;
+            }
+
+            operator = nameToSymbol(className.split(" ").at(-1));
+            if (operator !== "=") {
+                contentDiv.textContent += operator;
+                if (contentDiv.textContent.length === 1) {
+                    if (operator === "+") {
+                        operator = "";
+                    } else if (operator === "−") {
+                        firstNumberIsNegative = true;
+                        operator = "";
+                    }
+                }
             }
         } else {
             switch (className) {
@@ -59,9 +104,15 @@ function attachButtonEvents(containerDiv) {
                 
                 case "clear-entry":
                     contentDiv.textContent = contentDiv.textContent.slice(0, -1);
+                    if (!operator) {
+                        firstNumber = firstNumber.slice(0, -1);
+                    } else {
+                        secondNumber = secondNumber.slice(0, -1);
+                    }
                     break;
 
                 case "all-clear":
+                    wipeData();
                     contentDiv.textContent = "";
                     break;
             }
