b16b7b37b14216e4fce3a882296d5536c54e0d00
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-18 21:07:15 +0700

    Refactor event listeners

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 function initialize() {
     const containerDiv = document.querySelector("#container");
-    const optionsDiv = containerDiv.querySelector(".options");
 
-    optionsDiv.addEventListener("click", handleButtonClick);
+    attachButtonEvents(containerDiv);
 }
 
-function handleButtonClick(event) {
-    const target = event.target;
-    const containerDiv = target.closest("#container");
-    const displayDiv = containerDiv.querySelector(".display");
-    const contentDiv = displayDiv.firstElementChild;
-    const className = target.className;
+function attachButtonEvents(containerDiv) {
+    const optionsDiv = containerDiv.querySelector(".options");
 
-    if (className.startsWith("number")) {
-        contentDiv.textContent += className.slice(-1);
-    } else if (className.startsWith("operator")) {
-        if (className.endsWith("plus")) {
-            contentDiv.textContent += "+";
-        } else if (className.endsWith("minus")) {
-            contentDiv.textContent += "−";
-        } else if (className.endsWith("multiply")) {
-            contentDiv.textContent += "×";
-        } else if (className.endsWith("divide")) {
-            contentDiv.textContent += "÷";
-        }
-    } else {
-        switch (className) {
-            case "dot":
-                contentDiv.textContent += ".";
-                break;
-            
-            case "clear-entry":
-                contentDiv.textContent = contentDiv.textContent.slice(0, -1);
-                break;
+    optionsDiv.addEventListener("click", function (event) {
+        const target = event.target;
+        const containerDiv = target.closest("#container");
+        const displayDiv = containerDiv.querySelector(".display");
+        const contentDiv = displayDiv.firstElementChild;
+        const className = target.className;
 
-            case "all-clear":
-                contentDiv.textContent = "";
-                break;
+        if (className.startsWith("number")) {
+            contentDiv.textContent += className.slice(-1);
+        } else if (className.startsWith("operator")) {
+            if (className.endsWith("plus")) {
+                contentDiv.textContent += "+";
+            } else if (className.endsWith("minus")) {
+                contentDiv.textContent += "−";
+            } else if (className.endsWith("multiply")) {
+                contentDiv.textContent += "×";
+            } else if (className.endsWith("divide")) {
+                contentDiv.textContent += "÷";
+            }
+        } else {
+            switch (className) {
+                case "dot":
+                    contentDiv.textContent += ".";
+                    break;
+                
+                case "clear-entry":
+                    contentDiv.textContent = contentDiv.textContent.slice(0, -1);
+                    break;
+
+                case "all-clear":
+                    contentDiv.textContent = "";
+                    break;
+            }
         }
-    }
+    });
 }
 
 initialize();
